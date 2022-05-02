870208706281155176dd36487a8a5d38afc713fb
DESKTOP-UVAKAFJ\Cam <c.shkardoon@gmail.com>
2022-05-03 09:43:56 +1200

    seperated header/footer into their own .JS, and .CSS

---
+++ HeaderFooter/Header.js
@@ -0,0 +1,21 @@
+document.write('<head>  '  +
+     '<div class="pad">' +
+    '<div class="rowh">' +
+    '<div class="column1h">' +
+    '<img src="../Images/logo2.png" width="100%">' +
+    '</div>' +
+    '<div class="column2h">' +
+    '<div class="dropdown">' +
+    '<button class="dropbtn">' +
+    '<img src="../Images/burgerbun menu.png" width="30" height="30"></button>' +
+    '<div class="dropdown-content">' +
+    '<a href="../Home/Home.html">Home</a>' +
+    '<a href="../booking/Booking.html">Booking</a>' +
+    '<a href="../Details/Details.html">Details</a>' +
+    '<a href="../About/About.html">About Us</a>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</head>')
